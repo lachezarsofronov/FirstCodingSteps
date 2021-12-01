e14d422633a97423ae7610961330e9260814f690
Lachezar Sofronov <lachezarlachezarovsofronov@outlook.com>
2021-12-01 13:08:13 +0200

    One more test

--- NumsFromOneToTen/Program.cs
+++ NumsFromOneToTen/Program.cs
@@ -6,7 +6,10 @@ namespace NumsFromOneToTen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
