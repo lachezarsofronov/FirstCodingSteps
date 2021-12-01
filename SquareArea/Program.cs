7f870ad422930bd98739c89e18ed3b822bb838ff
Lachezar Sofronov <lachezarlachezarovsofronov@outlook.com>
2021-12-01 13:09:28 +0200

    test3

--- SquareArea/Program.cs
+++ SquareArea/Program.cs
@@ -6,7 +6,7 @@ namespace SquareArea
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Held!");
         }
     }
 }
