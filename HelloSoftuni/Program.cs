039872cee23cf31f95752456199cf6a79946fc2f
Lachezar Sofronov <lachezarlachezarovsofronov@outlook.com>
2021-12-01 13:01:09 +0200

    Made my first Hello SoftUni C# app.

--- HelloSoftuni/Program.cs
+++ HelloSoftuni/Program.cs
@@ -6,7 +6,7 @@ namespace HelloSoftuni
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello SoftUni!");
         }
     }
 }
