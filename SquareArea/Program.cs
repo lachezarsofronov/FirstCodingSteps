21efcd2f70168685fac68667d793bb1af2f37599
Lachezar Sofronov <lachezarlachezarovsofronov@outlook.com>
2021-12-02 10:18:22 +0200

    trerte

--- SquareArea/Program.cs
+++ SquareArea/Program.cs
@@ -6,7 +6,7 @@ namespace SquareArea
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helkojkl!");
+            Console.WriteLine("Hjkl!");
         }
     }
 }
